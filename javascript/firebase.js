dbca5f620bb268d0d9b47fb515209217bd29d5f7
nm-97 <nicomerz97@gmail.com>
2025-07-14 18:31:01 +0200

    delete the login.js

--- javascript/firebase.js
+++ javascript/firebase.js
@@ -17,3 +17,21 @@ document.addEventListener("DOMContentLoaded", function() {
     initializeUserData();
   }
 });
+
+function setGuestLogin() {
+  sessionStorage.setItem('currentUser', JSON.stringify({
+    type: "guest"
+  }));
+  window.location.href = "../html/summaryUser.html"; 
+}
+
+function addUser() {
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  user.push({
+    email: email.value,
+    password: password.value,
+    type: "registered"
+  });
+  window.location.href = "../html/index.html";
+}
