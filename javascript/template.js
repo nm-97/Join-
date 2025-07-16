6fecf24c46fd37b6af2ff78195b5cb4e81898bdb
nm-97 <nicomerz97@gmail.com>
2025-07-16 11:05:11 +0200

    set new colors for contactavatar

--- javascript/template.js
+++ javascript/template.js
@@ -146,7 +146,7 @@ function getContactTemplate(contact) {
 }
 
 function getAvatarColor(name) {
-  const colors = ["#FF8A00", "#9327FF", "#29ABE2", "#FF5EB3", "#6E52FF"];
+  const colors = ["#FF8A00", "#9327FF", "#29ABE2", "#FF5EB3", "#6E52FF", "#00BEE8", "#1FD7C1", "#FF745E", "#FFA35E", "#FC71FF", "#FFC701", "#0038FF", "#C3FF2B", "#FFE62B", "#FF4646", "#FFBB2B"];
   return colors[name.charCodeAt(0) % colors.length];
 }
 
