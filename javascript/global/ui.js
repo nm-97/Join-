6a0ba62353626325012fbc21367c6f776b73034c
nm-97 <nicomerz97@gmail.com>
2025-07-18 19:17:10 +0200

    show task bug fixes

--- javascript/global/ui.js
+++ javascript/global/ui.js
@@ -8,6 +8,23 @@ function closeUserMenu() {
   dropdown.style.display = "none";
 }
 
-function goBack() {
-    window.history.back();
+function showSuccessAddTaskMessage(params) {
+  const notificationHTML = getSuccessAddTaskMessageTemplate(params);
+  document.body.insertAdjacentHTML('beforeend', notificationHTML);
+  setTimeout(() => {
+    const notification = document.getElementById('taskNotification');
+    if (notification) {
+      notification.remove();
+    }
+  }, 2000);
+}
+
+async function createTask() {
+  const taskData = getFormData();
+  if (!validateTaskData(taskData)) {
+    return;
+  }
+  await addTaskToFirebase(taskData);
+  showSuccessAddTaskMessage({ message: 'Task successfully created!' });
+  clearForm();
 }
